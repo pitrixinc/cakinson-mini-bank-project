df23f9cbe70dcb2db267ef09e8a63937fb8644d5
pitrixinc <111375675+pitrixinc@users.noreply.github.com>
2023-04-11 09:28:21 +0000

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDni0dVyrpGNdGgA5Y5WoMBvwwzOxwX0gQ",
-  authDomain: "cakbank-ae708.firebaseapp.com",
-  projectId: "cakbank-ae708",
-  storageBucket: "cakbank-ae708.appspot.com",
-  messagingSenderId: "547169585150",
-  appId: "1:547169585150:web:4620a02999e895394ca2af"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
